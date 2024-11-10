259fb46f0deeb17ae2b7a1f088c5612e116b04ce
lizhihao51 <3212355224@qq.com>
2024-11-10 09:53:17 +0800

    更新 class.js
    修改逻辑，减少bug

--- Connections/class.js
+++ Connections/class.js
@@ -1,24 +1,46 @@
 var city = {
     0: ["请选择"],
-    1: ["请选择", "1班","2班", "3班", "4班", "5班", "6班", "7班", "8班", "9班", "10班"],
-    2: ["请选择","1班", "2班", "3班", "4班", "5班", "6班", "7班", "8班", "9班", "10班"],
-    3: ["请选择","1班", "2班", "3班", "4班", "5班", "6班", "7班", "8班", "9班", "10班"],
-}
+    1: ["请选择", "1班", "2班", "3班", "4班", "5班", "6班", "7班", "8班", "9班", "10班"],
+    2: ["请选择", "1班", "2班", "3班", "4班", "5班", "6班", "7班", "8班", "9班", "10班"],
+    3: ["请选择", "1班", "2班", "3班", "4班", "5班", "6班", "7班", "8班", "9班", "10班", "测试用班级"]
+};
 
-//js代码
 function test1() {
-    var xqSelect= document.getElementById("bj");
-    xqSelect.innerText="";
+    var xqSelect = document.getElementById("bj");
+    xqSelect.innerHTML = "";  // 清空班级下拉框
 
-    var selectValue = document.getElementById("nj").value;
-    var xqElement = city[selectValue];
-    for (var i=0;i<xqElement.length;i++) {
-        var text = document.createTextNode( xqElement[i]);
+    var selectValue = document.getElementById("nj").value;  // 获取选择的年级
+    var xqElement = city[selectValue];  // 根据年级获取对应的班级
+
+    // 动态生成班级选项
+    for (var i = 0; i < xqElement.length; i++) {
         var option = document.createElement("option");
-        option.appendChild(text);
-        xqSelect.appendChild(option);
+        option.value = xqElement[i];  // 设置选项的值
+        option.text = xqElement[i];   // 设置选项显示的文本
+        xqSelect.appendChild(option); // 添加到班级下拉框中
     }
 }
-function  init(){
-    test1();
+
+function init() {
+    test1(); // 页面加载时初始化班级选项
+    var savedGrade = localStorage.getItem("savedGrade");  // 从 localStorage 获取上次选择的年级
+    var savedClass = localStorage.getItem("savedClass");  // 获取上次选择的班级
+
+    if (savedGrade) {
+        document.getElementById("nj").value = savedGrade;  // 设置年级
+        test1();  // 更新班级选项
+
+        if (savedClass) {
+            document.getElementById("bj").value = savedClass;  // 设置班级
+        }
+    }
+}
+
+// 在选择年级时保存选择的年级和班级到 localStorage
+function saveSelection() {
+    var selectedGrade = document.getElementById("nj").value;
+    var selectedClass = document.getElementById("bj").value;
+
+    localStorage.setItem("savedGrade", selectedGrade);
+    localStorage.setItem("savedClass", selectedClass);
 }

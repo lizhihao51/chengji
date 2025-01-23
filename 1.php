2a18294a53d0aadfda50c93c144f6dbfc3148a18
lizhihao51 <3212355224@qq.com>
2025-01-23 15:20:39 +0800

    移动前必要备份

---
+++ 1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>权限不足</title>
+    <link href="style/register.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div>
+    <div class="tita">请重新输入</div>
+    <iframe src="//www.bilibili.com/blackboard/html5mobileplayer.html?isOutside=true&aid=651820362&bvid=BV1he4y1w7wB&cid=1006811391&p=1&autoplay=1&muted=0" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
+    <a id="tuichu" class="tita" href="login.php">返回上级&nbsp;</a>
+    </div>   
+    <div class="square">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>  
+        </div>
+        <div class="circle">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+</body>
+</html>
--- Start/2.php
+++ Start/2.php
@@ -26,12 +26,12 @@ $fun=$_COOKIE["fun"];
     if ($fun === '110') {
         // 显示 a 界面
         echo '<div class="xzk">
-                <a href="2/cs.php" target="kuang">班成绩单</a>
+                <a href="2/bj_rank.php" target="kuang">班成绩单</a>
               </div>';
     } elseif ($fun === '100') {
         // 显示 b 界面
         echo '<div class="xzk">
-                <a href="2/cs.php" target="kuang">班成绩单</a>
+                <a href="2/bj_rank.php" target="kuang">班成绩单</a>
               </div>';
     } else {
         // 默认显示内容
@@ -61,8 +61,17 @@ $fun=$_COOKIE["fun"];
     }
   ?>
     <div class="xzk">
-      <a href="2/jiagong.php" target="kuang">各科成绩</a>
+      <a href="2/personal_score.php" target="kuang">个人成绩</a>
     </div>
+
+    <div class="xzk">
+      <a href="2/user_psw.php" target="kuang">用户密码</a>
+    </div>
+
+    <div class="xzk">
+      <a href="2/user_rank.php" target="kuang">成绩</a>
+    </div>
+
     <div class="xzk">
       <a href="2/s_admin_rank.php?unam=<?php echo "$unam";?>" target="kuang">修改密码</a>
     </div>
@@ -71,7 +80,7 @@ $fun=$_COOKIE["fun"];
   
   <div id="right">
     <div id="main-bg">
-      <iframe id="kuang" src="student/t_xs_detail.php" scrolling="auto" name="kuang">
+      <iframe id="kuang" src="2/default.php" scrolling="auto" name="kuang">
         </iframe>
     </div>
   </div>

9cbb9514e7f3688e436101618fcc9282a70c0f45
lizhihao51 <3212355224@qq.com>
2025-04-20 20:54:52 +0800

    优化翻页

--- Start/2/user_psw.php
+++ Start/2/user_psw.php
@@ -2,15 +2,16 @@
 // 引入配置文件，假设该文件包含数据库连接信息
 require_once('../../Connections/login.php'); 
 require_once('../../Connections/is_login.php'); 
+require_once('../../Connections/pagination.php');
 
 // 初始化变量
 $currentPage = $_SERVER["PHP_SELF"];
-$maxRows_user = 20;
-$pageNum_user = 0;
-if (isset($_GET['pageNum_user'])) {
-    $pageNum_user = $_GET['pageNum_user'];
+$maxRows_cj_rank = 20;
+$pageNum_cj_rank = 0;
+if (isset($_GET['pageNum_cj_rank'])) {
+    $pageNum_cj_rank = $_GET['pageNum_cj_rank'];
 }
-$startRow_user = $pageNum_user * $maxRows_user;
+$startRow_cj_rank = $pageNum_cj_rank * $maxRows_cj_rank;
 
 // 获取搜索框的值
 $searchUsername = isset($_GET['username'])? $_GET['username'] : '';
@@ -28,37 +29,37 @@ if (!empty($searchUnam)) {
 }
 
 // 构建 SQL 查询语句
-$query_user = "SELECT * FROM user";
+$query_cj_rank = "SELECT * FROM user";
 if ($whereClause!= '1 = 1') {
-    $query_user.= " WHERE ". $whereClause;
+    $query_cj_rank.= " WHERE ". $whereClause;
 }
 
 // 先计算总记录数
-$allResult = mysql_query($query_user, $login);
+$allResult = mysql_query($query_cj_rank, $login);
 if (!$allResult) {
     die("总记录数查询失败: ". mysql_error());
 }
-$totalRows_user = mysql_num_rows($allResult);
-$totalPages_user = ceil($totalRows_user / $maxRows_user);
+$totalRows_cj_rank = mysql_num_rows($allResult);
+$totalPages_cj_rank = ceil($totalRows_cj_rank / $maxRows_cj_rank);
 
 // 再添加 LIMIT 子句进行分页查询
-$query_limit_user = $query_user. " LIMIT ". $startRow_user. ", ". $maxRows_user;
-
+$query_limit_cj_rank = $query_cj_rank. " LIMIT ". $startRow_cj_rank. ", ". $maxRows_cj_rank;
 // 执行分页查询
-$result = mysql_query($query_limit_user, $login);
+$result = mysql_query($query_limit_cj_rank, $login);
 if (!$result) {
     die("查询失败: ". mysql_error());
 }
 // 构建查询字符串，用于分页链接
-$queryString_user = '';
+$queryString_cj_rank = '';
 if (!empty($_GET)) {
     $param_pairs = [];
     foreach ($_GET as $key => $value) {
-        // 确保所有参数都包含在分页链接中
-        $param_pairs[] = urlencode($key). '='. urlencode($value);
+        if ($key!== 'pageNum_cj_rank') { // 排除 pageNum_cj_rank 参数，避免重复添加
+            $param_pairs[] = urlencode($key). '='. urlencode($value);
+        }
     }
     if (!empty($param_pairs)) {
-        $queryString_user = '?'. implode('&', $param_pairs);
+        $queryString_cj_rank = '&'. implode('&', $param_pairs);
     }
 }
 ?>
@@ -96,53 +97,31 @@ if (!empty($_GET)) {
         </div>
     </div> 
     <div id="jishu">
-        共有 <?php echo $totalRows_user ;?> 条记录，目前显示第 <?php echo ($startRow_user + 1);?> 条至第 <?php echo min($startRow_user + $maxRows_user, $totalRows_user);?> 条
+        共有 <?php echo $totalRows_cj_rank ;?> 条记录，目前显示第 <?php echo ($startRow_cj_rank + 1);?> 条至第 <?php echo min($startRow_cj_rank + $maxRows_cj_rank, $totalRows_cj_rank);?> 条
     </div>
-    <?php if ($totalRows_user == 0) :?>
+    <?php if ($totalRows_cj_rank == 0) :?>
         <div class="list2" style="text-align:center;">目前还没有添加任何信息</div>
     <?php else :?>
-        <?php while ($row_user = mysql_fetch_assoc($result)) :?>
+        <?php while ($row_cj_rank = mysql_fetch_assoc($result)) :?>
             <div class="list1">
                 <ul>
-                <li id="user"><?php echo empty($row_user['username'])? '-' : $row_user['username'];?></li>
-                <li id="psw"><?php echo empty($row_user['password'])? '-' : $row_user['password'];?></li>
-                <li><?php echo empty($row_user['unam'])? '-' : $row_user['unam'];?></li>
-                <li><?php echo empty($row_user['banji'])? '-' : $row_user['banji'];?></li>
-                <li><?php echo empty($row_user['level'])? '-' : $row_user['level'];?></li>
-                <li><?php echo empty($row_user['fun1'])? '-' : $row_user['fun1'];?></li>
-                <li><?php echo empty($row_user['fun2'])? '-' : $row_user['fun2'];?></li>
-                <li><?php echo empty($row_user['power'])? '-' : $row_user['power'];?></li>
-                <li><a href="edit_user.php?id=<?php echo urlencode($row_user['id']);?>">修改</a></li>
+                <li id="user"><?php echo empty($row_cj_rank['username'])? '-' : $row_cj_rank['username'];?></li>
+                <li id="psw"><?php echo empty($row_cj_rank['password'])? '-' : $row_cj_rank['password'];?></li>
+                <li><?php echo empty($row_cj_rank['unam'])? '-' : $row_cj_rank['unam'];?></li>
+                <li><?php echo empty($row_cj_rank['banji'])? '-' : $row_cj_rank['banji'];?></li>
+                <li><?php echo empty($row_cj_rank['level'])? '-' : $row_cj_rank['level'];?></li>
+                <li><?php echo empty($row_cj_rank['fun1'])? '-' : $row_cj_rank['fun1'];?></li>
+                <li><?php echo empty($row_cj_rank['fun2'])? '-' : $row_cj_rank['fun2'];?></li>
+                <li><?php echo empty($row_cj_rank['power'])? '-' : $row_cj_rank['power'];?></li>
+                <li><a href="edit_user.php?id=<?php echo urlencode($row_cj_rank['id']);?>">修改</a></li>
                 </ul>
             </div>
         <?php endwhile;?>
     <?php endif;?>
-    <div id="menu">
-        <?php if ($pageNum_user > 0 || $pageNum_user < $totalPages_user ) :?>
-            <div id="xzys">
-        <?php endif;?>
-        <?php if ($pageNum_user > 0) :?>
-            <a href="<?php printf("%s?pageNum_user=%d%s", $currentPage,  0, $queryString_user);?>"><img src="imgs/1.png" width="50px" height="50px"></a> 
-        <?php else :?>
-            <a><img src="imgs/w.png" width="50px" height="0px"></a>
-        <?php endif;?>
-        <?php if ($pageNum_user > 0) :?>
-            <a href="<?php printf("%s?pageNum_user=%d%s", $currentPage,  max(0, $pageNum_user - 1), $queryString_user);?>"><img src="imgs/2.png" width="50px" height="50px"></a> 
-        <?php else :?>
-            <a><img src="imgs/w.png" width="50px" height="0px"></a>
-        <?php endif;?>
-        <?php if ($pageNum_user < $totalPages_user) :?>
-            <a href="<?php printf("%s?pageNum_user=%d%s", $currentPage, min($totalPages_user, $pageNum_user + 1), $queryString_user);?>"><img src="imgs/3.png" width="50px" height="50px"></a> 
-        <?php else :?>
-            <a><img src="imgs/w.png" width="50px" height="0px"></a>
-        <?php endif;?>
-        <?php if ($pageNum_user < $totalPages_user) :?>
-            <a href="<?php printf("%s?pageNum_user=%d%s",  $currentPage, $totalPages_user, $queryString_user);?>"><img src="imgs/4.png" width="50px" height="50px"></a> 
-        <?php else :?>
-            <a><img src="imgs/w.png" width="50px" height="0px"></a>
-        <?php endif;?>
-        </div>
-    </div>
+    <?php
+    // 这里调用了 PaginationHelper 类的方法生成翻页链接，假设 PaginationHelper 类已定义
+    echo PaginationHelper::getPaginationLinks($currentPage, $pageNum_cj_rank, $totalPages_cj_rank, $queryString_cj_rank);
+    ?>
 </div>
 </body>
 </html>
